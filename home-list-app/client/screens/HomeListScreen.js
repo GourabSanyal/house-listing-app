1c316af3280ce0276d18829f1a95a9ce04029c94
gourab <biz.gourab@gmail.com>
2022-07-24 14:50:43 +0530

    redux set up

--- home-list-app/client/screens/HomeListScreen.js
+++ home-list-app/client/screens/HomeListScreen.js
@@ -1,15 +1,43 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+// Dependencies and libraries
+import { StyleSheet, Text, View, FlatList } from 'react-native'
+import React, {useEffect} from 'react'
+import { useDispatch, useSelector } from 'react-redux';
 
 //components
 import Card from '../component/Card';
 import { FloatingAction } from "react-native-floating-action";
+import * as houseAction from '../redux/actions/houseAction.js';
 
-const HomeListScreen = ({ navigation }) => {
+const HomeListScreen = ({ navigation, props }) => {
+
+  const dispatch = useDispatch();
+
+  const {houses} = useSelector((state) => state.house);
+
+  console.log(houses)
+  // console.log("Done")
+  
+  useEffect(() =>{
+    dispatch(houseAction.fetchHouses())
+  }, [dispatch])
+
   return (
     <View style={styles.container}>
-      <Card 
-        navigation={navigation}
+      <FlatList
+        data={houses}
+        keyExtractor={(item) => item._id}
+        renderItem={( item ) =>{
+          <Card 
+            navigation={navigation}
+            title={item.title}
+            address={item.address}
+            hometype={item.homeType}
+            description={item.description}
+            price={item.price}
+            image={item.image}
+            yearBuiilt={item.yearBuiilt}
+          />
+        }}
       />
       <FloatingAction 
         position="right"
