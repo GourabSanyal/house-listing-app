e7e993e73e27d6a7de5f80f8333f8ddc4d657cf8
Gourab Sanyal <sanyal.gourab@gmail.com>
2022-12-27 02:42:48 +0530

    tab navigation icon logic

--- home-list-app/client/navigation/AppNavigator.js
+++ home-list-app/client/navigation/AppNavigator.js
@@ -4,6 +4,7 @@ import React from "react";
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import { View } from 'react-native';
 
 // Screens
 import AboutScreen from "../screens/AboutScreen";
@@ -12,7 +13,7 @@ import HomeDetailsScreen from "../screens/HomeDetailsScreen";
 import HomeListScreen from "../screens/HomeListScreen"
 
 // assets
-import { MaterialIcons } from '@expo/vector-icons';
+import { MaterialIcons, Ionicons  } from '@expo/vector-icons';
 
 const Stack = createNativeStackNavigator();
 const Tab = createMaterialBottomTabNavigator();
@@ -41,7 +42,7 @@ function StackNavigator() {
 function AboutStackNavigator() {
     return (
         <Stack.Navigator>
-            <Stack.Screen name="About" component={AboutScreen}/>
+            <Stack.Screen name="About Screen" component={AboutScreen}/>
         </Stack.Navigator>
     )
 }
@@ -50,24 +51,33 @@ function AppsNavigator() {
     return(
         <NavigationContainer>
             <Tab.Navigator
-                screenOptions = {({ route }) => ({
-                    tabBarIcon: () => {
-                        let iconName;
-                        if ( route.name == "Home" ) {
-                            iconName: "home"
-                        } else if ( route.name == "About") {
-                            iconName: "info"
-                        }
-
-                        return <MaterialIcons name={iconName} size={24} />
-                    }
-                })}
+             screenOptions={({ route }) => ({
+                tabBarIcon: ({ focused, color, size }) => {
+                  let iconName;
+      
+                  if (route.name === 'Home') {
+                    iconName = focused
+                      ? 'home-sharp'
+                      : 'home-outline';
+                  } else if (route.name === 'About') {
+                    iconName = focused ? 'information-circle' : 'information-circle-outline';
+                  }
+      
+                  // You can return any component that you like here!
+                  return <Ionicons  name={iconName} size={29} color={color} />;
+                },
+              })}
+              tabBarOptions={{
+                activeTintColor: 'tomato',
+                inactiveTintColor: 'gray',
+              }}
             >
                 <Tab.Screen 
                     name="Home" 
-                    component={StackNavigator} />
+                    component={StackNavigator} 
+                />
                 <Tab.Screen 
-                    name = "AboutStack" 
+                    name = "About" 
                     component = {AboutStackNavigator}/>
             </Tab.Navigator>
         </NavigationContainer>
