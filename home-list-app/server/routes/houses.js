b1b41c252ec0986d362c082abe2c880b0c5676e6
Gourab Sanyal <sanyal.gourab@gmail.com>
2022-12-26 23:26:37 +0530

    end points created and updated

--- home-list-app/server/routes/houses.js
+++ home-list-app/server/routes/houses.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const { body, validationResult } = require('express-validator');
+const { body, validationResult, check } = require('express-validator');
 
 
 const House = require('../models/House');
@@ -9,16 +9,16 @@ const router = express.Router();
 
 // // pass validation in req data using express-validation lib
 const validate = [
-    body('title')
+    check('title')
         .isLength({ min: 3, max: 50 })
         .withMessage('Tilte should be betwwen 3 to 50 characters'),
-    body('description')
+    check('description')
         .isLength({ min: 10, max: 200 })
         .withMessage('Description should be betwwen 3 to 200 characters'),
-    body('address')
+    check('address')
         .isLength({ min: 10, max: 100 })
         .withMessage('Address should be betwwen 3 to 50 characters'),
-    body('price')
+    check('price')
         .isNumeric()
         .withMessage('Price should be a number'),
  ]
@@ -54,7 +54,7 @@ const validate = [
 
  })
 
-//  // /api/houses
+// get all the house data - /api/houses
  router.get('/', ( req, res ) => {
      House.find()
      .then(houses => {
@@ -63,54 +63,67 @@ const validate = [
         .catch(err => console.log(err))
     })
     
-// // /api/houses/id
-// router.get('/:id', ( req, res ) =>{
-//     const houseId = req.params.id;
+// Get a single house data using id - /api/houses/id
+router.get('/:id', ( req, res ) =>{
+    const houseId = req.params.id;
     
-//     House.findById(houseId)
-//     .then(house => {
-//         res.send(house)
-//     })
-//     .catch(err => console.group(err))
-// })
+    House.findById(houseId)
+    .then(house => {
+        res.send(house)
+    })
+    .catch(err => console.group(err))
+})
     
-// // /api/houses/id
-// router.put('/:id', validate, ( req, res ) =>{
-//     const houseId = req.params.id;
+// update an item - /api/houses/id
+router.put('/:id', validate, ( req, res ) =>{
+    const houseId = req.params.id;
 
-//     const errors = validationResult(req);
+    const errors = validationResult(req);
 
-//     if(!errors.isEmpty()){
-//         return res.status(422).send({ errors: errors.array()})
-//     }
+    if(!errors.isEmpty()){
+        return res.status(422).send({ errors: errors.array()})
+    }
 
-//     House.findById(houseId)
-//         .then(house=> {
-//             house.title = req.body.title;
-//             house.description = req.body.description;
-//             house.address = req.body.address;
-//             house.homeType = req.body.homeType;
-//             house.price = req.body.price;
-//             house.image = req.body.image;
-//             house.yearBuilt = req.body.yearBuilt;
+    House.findById(houseId)
+        .then(house=> {
+            house.title = req.body.title;
+            house.description = req.body.description;
+            house.address = req.body.address;
+            house.homeType = req.body.homeType;
+            house.price = req.body.price;
+            house.image = req.body.image;
+            house.yearBuilt = req.body.yearBuilt;
 
-//             return house.save()
-//         })
-//         .then(result => {
-//             res.send(result);
-//         })
-//         .catch(err => console.log(err))
-// }) 
+            return house.save()
+        })
+        .then(result => {
+            res.send(result);
+        })
+        .catch(err => console.log(err))
+})
 
-// // /api/houses/id
-// router.delete('/:id', validate, ( req, res) =>{
-//     const houseId = req.body.id;
+// deleting a house by id - /api/houses/id
 
-//     House.findByIdAndRemove(houseId)
-//     .then(result =>{
-//         res.send(result)
-//     })
-//     .catch(err => console.log(err))
+router.delete('/:id', validate, (req, res) => {
+    const houseId = req.params.id
+
+    House.findByIdAndDelete(houseId)
+        .then( result => {
+            res.send(result)
+        })
+        .catch( err =>  console.log(err) );
+});
+
+// router.delete('/:id', validate, ( req, res ) =>{
+    // const houseId = req.body.id;
+    // console.log(req.body.id)
+
+    // House.findByIdAndRemove(houseId)
+    // .then(result =>{
+    //     res.send(result)
+        
+    // })
+    // .catch(err => console.log(err))
 // })
 
 
