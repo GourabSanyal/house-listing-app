cfce396cf8891b72ed7f07290135c918d16bd5c7
Gourab Sanyal <sanyal.gourab@gmail.com>
2023-01-11 02:19:56 +0530

    minor

--- client/index.js
+++ client/index.js
@@ -1,8 +1,15 @@
 import { registerRootComponent } from 'expo';
+import { LogBox } from 'react-native';
 
 import App from './App';
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
+// Ignore log notification by message:
+LogBox.ignoreLogs(['You are now..']);
+
+// Ignore all log notifications:
+LogBox.ignoreAllLogs();
+
 registerRootComponent(App);
